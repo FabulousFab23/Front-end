127ee00301e0fc4c77307e5bba0dcf481ff80225
Sergey Amelin <sergey.amelin@resty-app.com>
2021-08-28 20:59:28 +0300

    Init project structure

---
+++ src/navigation/index.js
@@ -0,0 +1,30 @@
+import { StackActions} from '@react-navigation/native';
+import * as React from 'react';
+
+export const AppRoute = {
+  LOGIN:'Login',
+  SIGN_UP:'Sign Up',
+  FORGOT_PASSWORD:'Forgot Password',
+  HOME:"Home"
+};
+
+export const navigationRef = React.createRef();
+
+export const navigate = (name, params) => {
+  if (navigationRef.current) {
+    navigationRef.current.navigate(name, params);
+  }
+};
+
+export const goBack = () => {
+  navigationRef.current?.goBack();
+};
+
+export const replace = (name)  => {
+  navigationRef.current?.dispatch({
+    ...StackActions.replace(name),
+  });
+};
+
+export const getCurrentRoute = () =>
+  navigationRef.current?.getCurrentRoute() || null;
---
+++ src/navigation/router.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { AppRoute, navigate, navigationRef } from ".";
+import HomeScreen from "../screens/home-screen/home-screen";
+
+const Stack = createStackNavigator();
+
+const Router  = () => {
+
+return (
+  <NavigationContainer ref={navigationRef}>
+    <Stack.Navigator>
+          <Stack.Screen
+            name={AppRoute.HOME}
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+  </NavigationContainer>
+);
+};
+
+export default Router;
